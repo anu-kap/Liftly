a14d19481c946d2ed4180912c7f0373b63d159bc
anu-kap <burralenoda@gmail.com>
2026-06-12 16:07:50 -0700

    new

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -13,13 +13,24 @@
 // saved on the device, and your data is migrated to the cloud automatically
 // the first time you sign in.
 
-export const firebaseConfig = {
-  apiKey: 'PASTE_YOUR_API_KEY',
-  authDomain: 'PASTE_YOUR_PROJECT.firebaseapp.com',
-  projectId: 'PASTE_YOUR_PROJECT_ID',
-  storageBucket: 'PASTE_YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'PASTE_YOUR_SENDER_ID',
-  appId: 'PASTE_YOUR_APP_ID',
-}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const firebaseEnabled = !firebaseConfig.apiKey.startsWith('PASTE_')
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8G5Qq1ajcvgViLbdcN5vn0yilllyBcKU",
+  authDomain: "liftly-eb864.firebaseapp.com",
+  projectId: "liftly-eb864",
+  storageBucket: "liftly-eb864.firebasestorage.app",
+  messagingSenderId: "726083080358",
+  appId: "1:726083080358:web:bec646240bee73e11ae467",
+  measurementId: "G-12CP583QD8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
